1f367f8609db6a575b8ed0478b1c8539ccd5a74e
Kayla Gray <kjoygray75@gmail.com>
2024-05-02 21:12:05 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i8 = int8_t;      // -128 to 127
+using u8 = uint8_t;     // 0 to 255
+
+using i6 = int16_t;     // -32768 to 32767
+using u16 = uint16_t;   // 0 to 65535
+
+using i32 = int32_t;    // -2,147,483,648 to 2,147,483,647
+using u32 = uint32_t;   // 0 to 4,294,967,295
+
+using i64 = int64_t;    // -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+using u64 = uint64_t;   // 0 to 18,446,744,073,709,551,615
+
+using f = float;
+using d = double;
+using dl = long double;
+
+int main() {
+    // these lines make can improve IO speed
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+
+    return 0;
+}
